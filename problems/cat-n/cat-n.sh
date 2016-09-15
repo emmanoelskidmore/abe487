20ef780d3f5743470e17062edab3fbaec0b59ce1
Emma Noel Skidmore <emmanoelskidmore@login3.cm.cluster>
2016-09-15 12:08:50 -0700

    adding adding Problems

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+if [[ $# -lt 1 ]]; then 
+	echo "Usage: Must state file name"
+	exit 1
+fi
+
+FILE=$1
+
+
+i=0 
+
+	
+	while read FILE; do
+		
+		if [ $i -lt 2 ]; then
+	
+			let i++
+			echo $i $FILE		
+		fi
+		
+done < input.txt
+
+
